9cf57443bc4502787861d81e120814839f59f206
Pablo Rodriguez <hello@prodz.me>
2023-03-18 17:19:39 -0400

    PR feedback

--- app/routes/__layout/index.tsx
+++ app/routes/__layout/index.tsx
@@ -8,32 +8,34 @@ import { db } from "~/utils/db.server";
 import ModelsListComponent from "~/components/ModelList";
 
 type LoaderData = {
-	models: any;
-	user?: User | null;
-	count?: number;
-	page?: number;
-	hasMore?: boolean;
+  models: any;
+  user?: User | null;
+  count?: number;
+  page?: number;
+  hasMore?: boolean;
 };
 
 export const loader: LoaderFunction = async ({ request, context }) => {
-	const models = await db.model.findMany({
-		include: {
-			profile: true,
-		},
-	});
+  const models = await db.model.findMany({
+    include: {
+      profile: true,
+    },
+  });
 
-	return json<LoaderData>({
-		models: models,
-		user: null,
-	});
+  console.log(models);
+
+  return json<LoaderData>({
+    models: models,
+    user: null,
+  });
 };
 
 export default function Index() {
-	const data = useLoaderData<LoaderData>();
-	const [searchParams] = useSearchParams();
-	const location = useLocation();
-	const page = +(searchParams.get("page") ?? 0);
+  const data = useLoaderData<LoaderData>();
+  const [searchParams] = useSearchParams();
+  const location = useLocation();
+  const page = +(searchParams.get("page") ?? 0);
 
-	// WE ARE MAKING MODEL LIST THE DEFAULT FOR NOW
-	return <div>{<ModelsListComponent data={data} />}</div>;
+  // WE ARE MAKING MODEL LIST THE DEFAULT FOR NOW
+  return <div>{<ModelsListComponent data={data} />}</div>;
 }
