a165ec99fb8df6486732bed0dd41dbb513844845
Pablo Rodriguez <hello@prodz.me>
2023-03-18 22:57:33 -0400

    more layout changes to Model List component

--- app/components/ModelList.tsx
+++ app/components/ModelList.tsx
@@ -1,99 +1,105 @@
-import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { useLoaderData, useLocation, Form, useSearchParams } from "@remix-run/react";
-import Table from "~/components/ui/Table";
-import ModelEntryActions from "~/components/ModelEntryActions";
-import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
 import Button from "~/components/ui/Button";
 import ButtonLink from "~/components/ui/ButtonLink";
 import * as timeago from "timeago.js";
-import Input from "~/components/ui/Input";
 
 const ModelsListComponent = ({ data }: any) => {
-	const results = (
-		<Table
-			columns={[
-				{
-					title: "Model Name",
-					renderCell: (model: any) => {
-						return (
-							<div className="flex items-center">
-								<details>
-									<summary className="cursor-pointer">
-										<span className="pl-2">{model?.title}</span>
-									</summary>
+  const renderModelItem = (model) => {
+    return (
+      <div className="flex-1 p-5 bg-stone-800 text-white mb-5 rounded-md">
+        <div className="flex">
+          <div className="w-3/4">
+            <div className="flex flex-col">
+              <div className="flex-1">
+                <div className="flex align-middle">
+                  <div className="w-10 h-10 inline-block mr-2 border border-gray-600 rounded-md">Icon</div>
+                  <span className="font-bold text-3xl">{model.title}</span>
+                </div>
+              </div>
+              <div className="flex-1">
+                <span className="inline-block mr-4">{model.profile.username}</span>
+                <span className="inline-block mr-4">{timeago.format(new Date(model?.createdAt!))}</span>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/4 pl-4">
+            <div className="flex items-center h-full">
+              <div className="flex-1">
+                <div className="flex justify-end">
+                  <Button type="button" variant="link" className="ml-8">
+                    Favorite
+                  </Button>
+                  <Button type="button" variant="link" className="ml-8">
+                    Download
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
-									<div className="p-3">
-										<h4 className="font-bold">Amp</h4>
-										<div>{model?.ampName}</div>
+  return (
+    <div>
+      <div className="flex">
+        {/* SORT AREA */}
+        <div className="flex-none mb-8">
+          <Button type="button" variant="link" className="mr-8">
+            Newest
+          </Button>
+          <Button type="button" variant="link" className="mr-8">
+            Popular
+          </Button>
+          <Button type="button" variant="link" className="mr-8">
+            Own Models
+          </Button>
+        </div>
 
-										<h4 className="font-bold pt-3">Description</h4>
-										<div>{model?.description}</div>
-									</div>
-								</details>
-							</div>
-						);
-					},
-				},
-				{
-					title: "User",
-					className: "w-64",
-					renderCell: (model) => {
-						return <>{model?.profile?.username}</>;
-					},
-				},
-				{
-					title: "Uploaded",
-					className: "w-32",
-					renderCell: (model) => {
-						return <>{timeago.format(new Date(model?.createdAt!))}</>;
-					},
-				},
-				{
-					title: "Download",
-					className: "w-10",
-					renderCell: (model) => {
-						return (
-							<div className="flex items-center">
-								<a href={`/models/${model.id}/download`} download className="inline-block">
-									<ArrowDownTrayIcon className="w-5 h-5" />
-								</a>
-								{data.user?.id === model.profile_id ? (
-									<div className="ml-3">
-										<ModelEntryActions model={model} />
-									</div>
-								) : null}
-							</div>
-						);
-					},
-				},
-			]}
-			data={data.models ?? []}
-		/>
-	);
+        {/* TAGS AREA */}
+        <div className="flex-grow justify-end">
+          <div className="flex justify-end">
+            <Button type="button" variant="link" className="mr-4">
+              Tag 1
+            </Button>
+            <Button type="button" variant="link" className="mr-4">
+              Tag 2
+            </Button>
+            <Button type="button" variant="link" className="mr-4">
+              Tag 3
+            </Button>
+            <Button type="button" variant="link" className="mr-4">
+              Tag 4
+            </Button>
+            <Button type="button" variant="link" className="mr-4">
+              Tag 5
+            </Button>
+          </div>
+        </div>
+      </div>
+      {/* <div className="flex">
+        <Form method="get" action="/" className="flex-grow text-center px-10">
+          <Input
+            name="search"
+            placeholder="Enter Search ..."
+            className="inline-block mr-3"
+            style={{ maxWidth: "420px" }}
+          />
+          <Button type="submit">Search</Button>
+        </Form>
+      </div> */}
 
-	return (
-		<div>
-			<nav className="max-w-6xl m-auto mb-16 mt-16">
-				<Form method="get" action="/" className="flex-grow text-center px-10">
-					<Input
-						name="search"
-						placeholder="Enter Search ..."
-						className="inline-block mr-3"
-						style={{ maxWidth: "420px" }}
-					/>
-					<Button type="submit">Search</Button>
-				</Form>
-			</nav>
-			<div className="flex flex-col max-w-6xl m-auto">
-				<div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-					<div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-						<div className="overflow-hidden">{data.models?.length === 0 ? <>No results</> : results}</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+      {/* MODELS LIST */}
+      <div className="flex flex-col">
+        {data.models?.length === 0 ? <span>No results</span> : null}
+        {data.models?.length > 0 ? data.models.map((model) => renderModelItem(model)) : null}
+      </div>
+      {/* PAGINATION AREA */}
+      <div className="flex justify-end">
+        <span>Pagination Area</span>
+      </div>
+    </div>
+  );
 };
 
 export default ModelsListComponent;
--- app/routes/__layout/index.tsx
+++ app/routes/__layout/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData, useLocation, useSearchParams } from "@remix-run/react";
@@ -34,8 +35,35 @@ export default function Index() {
   const data = useLoaderData<LoaderData>();
   const [searchParams] = useSearchParams();
   const location = useLocation();
-  const page = +(searchParams.get("page") ?? 0);
+  //const page = +(searchParams.get("page") ?? 0);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(0);
+  const [sort, setSort] = useState<string>("newest");
 
   // WE ARE MAKING MODEL LIST THE DEFAULT FOR NOW
-  return <div>{<ModelsListComponent data={data} />}</div>;
+  return (
+    <div className="w-full">
+      <div className="flex">
+        <h1 className="text-5xl font-bold mb-10">Browse NAM Models</h1>
+      </div>
+      <div className="flex">
+        <div className="w-3/4">
+          {loading ? "Loading..." : null}
+          {!loading ? <ModelsListComponent data={data} /> : null}
+        </div>
+        <div className="w-1/4 px-4">
+          <div className="w-full text-white rounded-md p-2 mb-8 border border-gray-600">
+            <span className="block p-20 text-center">Block 1</span>
+          </div>
+          <div className="w-full text-white rounded-md p-2 mb-8 border border-gray-600">
+            <span className="block p-20 text-center">Block 2</span>
+          </div>
+          <div className="w-full text-white rounded-md p-2 mb-8 border border-gray-600">
+            <span className="block p-20 text-center">Block 3</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
