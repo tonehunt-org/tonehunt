9b95bb1cb97d729cd17caaf7953989bd0857d293
Pablo Rodriguez <hello@prodz.me>
2023-03-27 15:51:04 -0400

    added Articles block

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -2,6 +2,9 @@ import { StarIcon, UserCircleIcon, UserIcon, RectangleStackIcon } from "@heroico
 import { Link } from "@remix-run/react";
 import type { User } from "@supabase/supabase-js";
 import { map } from "lodash";
+import imgArticle1 from "~/assets/articles/article_1.jpg";
+import imgArticle2 from "~/assets/articles/article_2.jpg";
+import imgArticle3 from "~/assets/articles/article_3.jpg";
 
 type SidebarProps = {
   user?: User | null | undefined;
@@ -35,7 +38,7 @@ const Sidebar = ({ user, username }: SidebarProps) => {
           </div>
           <div className="flex-1">
             <div className="flex flex-row items-center">
-              <Link to="/account/profile" prefetch="intent" className="hover:underline">
+              <Link to={`/${username}`} prefetch="intent" className="hover:underline">
                 <UserCircleIcon className="inline w-5 h-5 mr-2" />
                 <span className="font-satoshi-regular text-sm">View Profile</span>
               </Link>
@@ -75,6 +78,58 @@ const Sidebar = ({ user, username }: SidebarProps) => {
     );
   };
 
+  const ArticlesBlock = () => {
+    return (
+      <div className="w-full">
+        <div className="flex">
+          <h1 className="font-satoshi-bold text-xl mb-4">Tips &amp; Tricks</h1>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex-1 border-b border-gray-600 pb-3 mb-3">
+            <a href="https://github.com/sdatkinson/neural-amp-modeler" target="_new">
+              <div className="flex flex-row items-center">
+                <div className="inline w-14 h-14 mr-2 bg-tonehunt-purple rounded-xl overflow-hidden flex-none">
+                  <img
+                    className="object-cover w-14 h-14"
+                    src={imgArticle1}
+                    alt="capture_image"
+                    title="How to capture your own rig"
+                  />
+                </div>
+                <span className="font-satoshi-bold text-sm">How to capture your own rig</span>
+              </div>
+            </a>
+          </div>
+          <div className="flex-1 border-b border-gray-600 pb-3 mb-3">
+            <a href="https://github.com/sdatkinson/neural-amp-modeler" target="_new">
+              <div className="flex flex-row items-center">
+                <div className="inline w-14 h-14 mr-2 bg-tonehunt-orange rounded-xl overflow-hidden flex-none">
+                  <img
+                    className="object-cover w-14 h-14"
+                    src={imgArticle2}
+                    alt="train_image"
+                    title="How to train a model"
+                  />
+                </div>
+                <span className="font-satoshi-bold text-sm">How to train a model</span>
+              </div>
+            </a>
+          </div>
+          <div className="flex-1 border-b border-gray-600 pb-3 mb-3">
+            <a href="https://github.com/sdatkinson/NeuralAmpModelerPlugin" target="_new">
+              <div className="flex flex-row items-center">
+                <div className="inline w-14 h-14 mr-2 bg-tonehunt-green rounded-xl overflow-hidden flex-none">
+                  <img className="object-cover w-14 h-14" src={imgArticle3} alt="nam_image" title="Download NAM" />
+                </div>
+                <span className="font-satoshi-bold text-sm">Download the Neural Amp Modeling Plugin</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="w-full p-4">
       {user?.id ? (
@@ -82,11 +137,11 @@ const Sidebar = ({ user, username }: SidebarProps) => {
           <UserBlock />
         </div>
       ) : null}
-      <div className="w-full text-white p-2 mb-8">
+      <div className="w-full text-white mb-8">
         <TagsBlock />
       </div>
-      <div className="w-full text-white rounded-xl p-2 mb-8 border border-gray-600">
-        <span className="block p-20 text-center">ARTICLES</span>
+      <div className="w-full text-white mb-8">
+        <ArticlesBlock />
       </div>
     </div>
   );
