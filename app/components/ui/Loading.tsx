2738c0d07df777ddd57e8b9b1546b480924f395f
Pablo Rodriguez <hello@prodz.me>
2023-03-28 15:58:05 -0400

    fixed some warnings in svg components

--- app/components/ui/Loading.tsx
+++ app/components/ui/Loading.tsx
@@ -5,9 +5,9 @@ type LoadingProps = {
 export default function Loading({ size = "24" }: LoadingProps) {
   return (
     <svg width={size} height={size} viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
-      <g fill="none" fill-rule="evenodd">
-        <g transform="translate(1 1)" stroke-width="2">
-          <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
+      <g fill="none" fillRule="evenodd">
+        <g transform="translate(1 1)" strokeWidth="2">
+          <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
           <path d="M36 18c0-9.94-8.06-18-18-18">
             <animateTransform
               attributeName="transform"
