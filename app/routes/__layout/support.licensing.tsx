0e1082cdb31dfb3e354f1b13d1c76fffd6a361f4
Scott Corgan <scott@tamaracksoftware.com>
2023-04-07 09:27:49 -0400

    Update text style on license

--- app/routes/__layout/support.licensing.tsx
+++ app/routes/__layout/support.licensing.tsx
@@ -2,12 +2,12 @@ import { Link } from "@remix-run/react";
 
 export default function LicensingInfoPage() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col text-lg leading-relaxed">
       <div className="w-full">
-        <h1 className="w-full text-center text-2xl lg:text-3xl font-satoshi-bold mb-10">Licensing</h1>
+        <h2 className="text-4xl font-satoshi-bold pb-10">Licensing</h2>
       </div>
       <div className="w-full">
-        <p className="leading-loose text-justify">
+        <p className="leading-loose">
           Licensing can be a complicated issue. However, we have tried to make it as simple as possible. If you are
           here, chances are you want to share your AI models and presets. However, you may want control over what can be
           done with those models and presets. We recommend you check out{" "}
@@ -22,18 +22,18 @@ export default function LicensingInfoPage() {
           is often a good choice as it's one of the least restrictive. Alternatively you can consider our{" "}
           <span className="inline font-satoshi-bold text-tonehunt-yellow">Tone Hunt</span> license, this specifies that:
         </p>
-        <p className="leading-loose text-justify mt-5 bg-tonehunt-gray-medium p-5 rounded-lg font-satoshi-light italic">
+        <p className="leading-loose mt-10 bg-tonehunt-gray-medium p-5 rounded-lg font-satoshi-light italic">
           “The user may download and load the data file into software, and utilize or publish the outputs from said
           software resulting from use of the aforementioned data file, without royalty or restriction. The user may not
           upload, or otherwise republish, or distribute the data file, without express permission from the author of the
           aforementioned data file”.
         </p>
-        <p className="leading-loose text-justify mt-5">
+        <p className="leading-loose mt-10">
           Please note, Tone Hunt does not, and will never, take ownership of the content on this website. We are a free
           and open source community, dedicated to hosting resources for musicians. Authors (uploaders) retain whichever
           rights they wish when uploading to Tone Hunt.
         </p>
-        <p className="leading-loose text-justify mt-5">
+        <p className="leading-loose mt-10">
           More information:{" "}
           <Link
             to="https://creativecommons.org/about/cclicenses/"
