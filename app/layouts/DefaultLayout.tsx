ef52c99db2e37ac173c14d9a66b10c2003855aa0
Scott Corgan <scott@tamaracksoftware.com>
2023-04-18 06:53:51 -0400

    user related dropdown

--- app/layouts/DefaultLayout.tsx
+++ app/layouts/DefaultLayout.tsx
@@ -5,7 +5,7 @@ import type { User } from "@supabase/supabase-js";
 import type { Profile } from "@prisma/client";
 import type { PropsWithChildren } from "react";
 import { getSampleTags } from "~/services/tags";
-import { Link, NavLink } from "@remix-run/react";
+import { Form, Link, NavLink } from "@remix-run/react";
 import { twMerge } from "tailwind-merge";
 import {
   ListBulletIcon,
@@ -19,6 +19,7 @@ import {
   HomeIcon,
 } from "@heroicons/react/24/outline";
 import Button from "~/components/ui/Button";
+import RelatedDropdown from "~/components/RelatedDropdown";
 
 interface DefaultLayoutType {
   user?: User | null | undefined;
@@ -31,27 +32,31 @@ const nav1 = [
     title: "Following",
     href: "/",
     icon: HomeIcon,
+    requiresAuth: false,
   },
   {
     title: "Favorites",
     href: "/account/my-favorites",
     icon: StarIcon,
+    requiresAuth: true,
   },
   {
     title: "My Models",
     href: "/account/my-models",
+    requiresAuth: true,
     icon: QueueListIcon,
   },
 ];
 
 const nav2 = [
   { title: "Newest", href: "/newest", icon: ClockIcon },
-  { title: "Popular", href: "/popular", icon: ChartBarIcon },
   { title: "Trending", href: "/trending", icon: ArrowTrendingUpIcon },
+  { title: "Popular", href: "/popular", icon: ChartBarIcon },
 ];
 
 const DefaultLayout = (props: PropsWithChildren<DefaultLayoutType>) => {
   const { user, profile } = props;
+  console.log("profile", profile);
 
   return (
     <div className="">
@@ -62,34 +67,45 @@ const DefaultLayout = (props: PropsWithChildren<DefaultLayoutType>) => {
             className="list-none p-0 m-0 w-[240px] pt-5 font-satoshi-medium flex flex-col"
             style={{ height: "calc(100vh - 130px)" }}
           >
-            {nav1.map((item) => {
-              return (
-                <li key={item.href} className="my-1">
-                  <NavLink
-                    to={item.href}
-                    className={({ isActive }) => {
-                      return twMerge(
-                        "block px-5 py-2 rounded-full text-lg",
-                        isActive
-                          ? "text-tonehunt-purple"
-                          : "hover:text-white text-white/80 hover:bg-tonehunt-gray-medium"
-                      );
-                    }}
-                  >
-                    <span className="flex items-center gap-3">
-                      <item.icon className="w-5 h-5" /> {item.title}
-                    </span>
-                  </NavLink>
-                </li>
-              );
-            })}
-            <li className="pt-5" />
+            {nav1
+              .filter((item) => {
+                if (item.requiresAuth && !user) {
+                  return false;
+                }
+
+                return true;
+              })
+              .map((item) => {
+                return (
+                  <li key={item.href} className="my-1">
+                    <NavLink
+                      to={item.href}
+                      prefetch="intent"
+                      className={({ isActive }) => {
+                        return twMerge(
+                          "block px-5 py-2 rounded-full text-lg",
+                          isActive
+                            ? "text-tonehunt-purple"
+                            : "hover:text-white text-white/80 hover:bg-tonehunt-gray-medium"
+                        );
+                      }}
+                    >
+                      <span className="flex items-center gap-3">
+                        <item.icon className="w-5 h-5" /> {item.title}
+                      </span>
+                    </NavLink>
+                  </li>
+                );
+              })}
+
+            {!user ? <></> : <li className="pt-5" />}
 
             {nav2.map((item) => {
               return (
                 <li key={item.href} className="my-1">
                   <NavLink
                     to={item.href}
+                    prefetch="intent"
                     className={({ isActive }) => {
                       return twMerge(
                         "block px-5 py-2 rounded-full text-lg",
@@ -107,40 +123,49 @@ const DefaultLayout = (props: PropsWithChildren<DefaultLayoutType>) => {
               );
             })}
 
-            <li className="fixed bottom-0 pb-10 px-5 cursor-pointer bg-[#141414]">
-              <div className="flex items-center">
-                <Link
-                  to={`/${"one"}`}
-                  className="flex items-center hover:bg-tonehunt-gray-medium rounded-full pr-2 pl-2 relative -left-2 py-2"
-                >
-                  <div>
-                    <UserIcon className="block w-8 h-8 rounded-full p-2 bg-tonehunt-green mr-4" />
-                  </div>
-                  <div className="flex-grow truncate max-w-[120px]">@scottcorgan</div>
-                </Link>
+            {user && profile ? (
+              <li className="fixed bottom-0 pb-10 px-5 cursor-pointer bg-[#141414]">
+                <div className="flex items-center">
+                  <Link
+                    to={`/${profile?.username}`}
+                    className="flex items-center hover:bg-tonehunt-gray-medium rounded-full pr-2 pl-2 relative -left-2 py-2"
+                  >
+                    <div>
+                      <UserIcon className="block w-8 h-8 rounded-full p-2 bg-tonehunt-green mr-4" />
+                    </div>
+                    <div className="flex-grow truncate max-w-[120px]">@{profile.username}</div>
+                  </Link>
 
-                <Button
-                  variant="link"
-                  className="text-white/80 hover:bg-tonehunt-gray-medium rounded-full py-2 px-1 ml-2"
-                >
-                  <EllipsisVerticalIcon className="w-6 h-6" />
-                </Button>
-              </div>
-            </li>
+                  <RelatedDropdown className="cursor-default">
+                    <ul className="list-none p-0 m-0 flex flex-col gap-2">
+                      <li>
+                        <Link to="/account/profile" prefetch="intent" className="hover:underline">
+                          Edit Profile
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/account/change-password" prefetch="intent" className="hover:underline">
+                          Change Password
+                        </Link>
+                      </li>
+                      <li>
+                        <Form method="post" action="/logout">
+                          <Button variant="link" className="hover:underline">
+                            Logout
+                          </Button>
+                        </Form>
+                      </li>
+                    </ul>
+                  </RelatedDropdown>
+                </div>
+              </li>
+            ) : null}
           </ul>
         </nav>
         <div className="w-full max-w-3xl mb-8 mt-8 lg:mb-16 lg:mt-5 xl:mb-16 px-3">{props.children}</div>
 
         <nav className="flex-1 ml-5"></nav>
-        {/* <div className="w-full lg:w-1/4 xl:hidden">
-          <Sidebar user={user} username={profile?.username} tags={getSampleTags()} />
-        </div> */}
       </div>
-
-      {/* FIXED DESKTOP SIDEBAR */}
-      {/* <div className="hidden xl:block absolute top-20 right-0 w-72 min-h-full mb-20">
-        <Sidebar user={user} username={profile?.username} tags={getSampleTags()} />
-      </div> */}
 
       <Footer />
     </div>
