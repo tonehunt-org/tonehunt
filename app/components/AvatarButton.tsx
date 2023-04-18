9590b7f15331b0035353ee70171fc771eed6ea7a
Pablo Rodriguez <hello@prodz.me>
2023-04-18 17:43:42 -0400

    minor UI changes

--- app/components/AvatarButton.tsx
+++ app/components/AvatarButton.tsx
@@ -73,8 +73,8 @@ const AvatarButton = ({ profile }: any) => {
         </div>
       ) : null}
       {isFileUploading ? (
-        <div className="block w-full min-h-[112px] lg:min-h-[224px] border-white/30 rounded-lg border-dashed border-2">
-          <div className="flex flex-col items-center justify-center mb-5 px-8 py-10  lg:px-32 lg:py-20">
+        <div className="block w-full max-w-[320px] min-h-[224px] m-auto border-white/30 rounded-lg border-dashed border-2">
+          <div className="flex flex-col items-center justify-center mb-5 px-8 py-20">
             <Loading />
             <h3 className="font-satoshi-medium uppercase text-white/60 text-xs text-center mt-2">Image uploading</h3>
           </div>
@@ -83,7 +83,7 @@ const AvatarButton = ({ profile }: any) => {
       {!isFileUploading ? (
         <div
           className={twMerge(
-            "block w-full border-2 border-white rounded-lg px-8 lg:px-32 py-8 border-dashed overflow-hidden relative",
+            "block w-full max-w-[320px] m-auto border-2 border-white rounded-lg px-8 py-4 border-dashed overflow-hidden relative",
             drag ? "opacity-100" : "opacity-30",
             "hover:opacity-80"
           )}
@@ -116,20 +116,22 @@ const AvatarButton = ({ profile }: any) => {
 
               {isAvatar ? (
                 <img
-                  className="w-24 h-24 object-cover mb-5 rounded-full pointer-events-none"
+                  className="w-24 h-24 object-cover mb-3 rounded-full pointer-events-none"
                   src={profile.avatar}
                   title="Change avatar"
                   alt="Change avatar"
                 />
               ) : (
-                <UserPlusIcon className="h-24 w-24 mb-5 pointer-events-none" />
+                <UserPlusIcon className="h-24 w-24 mb-3 pointer-events-none" />
               )}
 
               <span className="pointer-events-none text-center">
                 Drop your image here or click to add/change your avatar.
               </span>
               <div className="text-xs text-center mt-1 text-white/60">
-                Recommended: 500 x 500 jpg or png format. Max size: 4mb.
+                Recommended: 500 x 500 jpg or png format.
+                <br />
+                Max size: 4mb.
               </div>
             </div>
           </avatarFetcher.Form>
