01ef1baac4ed4abbd98560da78d97d5d68f906eb
Pablo Rodriguez <hello@prodz.me>
2023-04-20 23:50:00 -0400

    fix for logging out

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,11 +1,22 @@
 import type { ActionFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { getSession } from "~/auth.server";
+import { createCookie } from "@remix-run/node";
 
 export const action: ActionFunction = async ({ request, context }) => {
-  const { supabase, response } = await getSession(request);
+  const { supabase } = await getSession(request);
 
   await supabase.auth.signOut();
 
-  return redirect("/", { headers: response.headers });
+  const cookie = createCookie("supabase-auth-token", {
+    expires: new Date(0),
+  });
+
+  return redirect("/", {
+    headers: {
+      "Set-Cookie": await cookie.serialize({
+        expires: new Date(0),
+      }),
+    },
+  });
 };
