3ba70418005deb188c7a32df8f33fb0b5587b21d
Pablo Rodriguez <hello@prodz.me>
2024-03-24 13:32:36 -0400

    added recaptcha on register form

--- app/components/UserActions.tsx
+++ app/components/UserActions.tsx
@@ -1,9 +1,10 @@
-import { Popover } from "@headlessui/react";
-import { Form, Link, useLocation } from "@remix-run/react";
+//import { Popover } from "@headlessui/react";
+//import { Form, Link, useLocation } from "@remix-run/react";
 import type { User } from "@supabase/supabase-js";
-import Button from "./ui/Button";
-import { ChevronDownIcon } from "@heroicons/react/24/solid";
-import LoginForm from "./LoginForm";
+//import Button from "./ui/Button";
+import ButtonLink from "./ui/ButtonLink";
+//import { ChevronDownIcon } from "@heroicons/react/24/solid";
+//import LoginForm from "./LoginForm";
 
 type UserActionsProps = {
   user?: User | null | undefined;
@@ -11,18 +12,21 @@ type UserActionsProps = {
 };
 
 export default function UserActions({ user, username }: UserActionsProps) {
-  const location = useLocation();
+  ////const location = useLocation();
 
   return user ? (
     <></>
   ) : (
-    <Popover className="hidden relative xl:block">
-      <Popover.Button as={Button} variant="primary">
-        Login or Sign Up
-      </Popover.Button>
-      <Popover.Panel className="absolute right-0  z-50 bg-zinc-900 border border-white/5 drop-shadow-xl rounded-xl p-5 w-72 shadow-lg">
-        <LoginForm redirectTo={`${location.pathname}${location.search}`} />
-      </Popover.Panel>
-    </Popover>
+    // <Popover className="hidden relative xl:block">
+    //   <Popover.Button as={Button} variant="primary">
+    //     Login or Sign Up
+    //   </Popover.Button>
+    //   <Popover.Panel className="absolute right-0  z-50 bg-zinc-900 border border-white/5 drop-shadow-xl rounded-xl p-5 w-72 shadow-lg">
+    //     <LoginForm redirectTo={`${location.pathname}${location.search}`} />
+    //   </Popover.Panel>
+    // </Popover>
+    <ButtonLink to="/login" variant="button-primary">
+      Login or Sign Up
+    </ButtonLink>
   );
 }
